1525a509994e0c88fd7d01dee78d7bd6a96452fc
Nathaniel Christy <80506862+Nathanielchristy@users.noreply.github.com>
2021-12-31 18:03:55 +0530

    First

---
+++ app.js
@@ -0,0 +1,64 @@
+const express = require('express'); 
+const path = require ('path'); 
+const cors = require('cors');
+
+const app = new express; 
+
+app.set('view engine','ejs'); 
+app.set('views','./src/views'); 
+
+app.use(cors());
+app.use(express.urlencoded({extended:true}));
+app.use(express.json());
+app.use(express.static(path.join(__dirname , '/public'))); 
+const nav= [
+    {
+        link:"/books",
+        title:"Books"
+    },
+    {
+        link:"/authors",
+        title:"Authors"
+    },
+    {
+        link:"/addbook",
+        title:"Add Book"
+    },
+    {
+        link:"/addauthor",
+        title:"Add Author"
+    }
+]
+
+const loginRouter = require('./src/routes/loginroute');
+const signupRouter = require('./src/routes/signuproute');
+const homeRouter = require('./src/routes/homerouter');
+const booksRouter = require('./src/routes/booksroute');
+const authorsRouter = require('./src/routes/authorsroute');
+
+
+
+
+
+
+app.use('/login',loginRouter); 
+app.use('/signup',signupRouter); 
+app.use('/home',homeRouter); 
+app.use('/books',booksRouter); 
+app.use('/authors',authorsRouter); 
+
+
+
+app.get('/',function(req,res){
+
+    res.render('index',{});
+    
+});
+
+
+
+
+//Port 3000
+app.listen(3000,()=>{
+    console.log("Server Ready on 3000");
+});
